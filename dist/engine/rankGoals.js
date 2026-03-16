697e39b36c36c9358c361c725bf1677ff91c454c
ERRLKing <jeranamo+errlking@gmail.com>
2026-03-16 16:46:23 -0700

    cleanup

--- dist/engine/rankGoals.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.rankGoals = rankGoals;
-function rankGoals(goals) {
-    return [...goals].sort((a, b) => b.priority - a.priority);
-}
--- dist/llm/parseModelJson.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.parseModelJson = parseModelJson;
-function parseModelJson(raw) {
-    return JSON.parse(raw);
-}
